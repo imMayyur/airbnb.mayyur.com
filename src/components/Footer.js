bc89d3d63fa98510ce07c4a77de4f7a58c6c47c6
Mayur Patel <programmer.mayur@gmail.com>
2021-08-06 00:11:49 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-sm text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+
+            <div className="space-y-4 text-sm text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Its a pretty awesome clone</p>
+            </div>
+
+            <div className="space-y-4 text-sm text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Mayur Patel</p>
+                <p>Presents</p>
+                <p>Full Stack Hero</p>
+                <p>Hundreads of Projects</p>
+            </div>
+            
+            <div className="space-y-4 text-sm text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Center</p>
+                <p>Trust & Safety</p>
+                <p>Say Hello</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer
