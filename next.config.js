9d4c6d2ee4863c75b3209da0daf9993093a38a31
Mayur Patel <programmer.mayur@gmail.com>
2021-08-07 02:31:36 +0530

    MAP

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    images: {
-        domains: ['links.papareact.com']
-    }
-}
+  images: {
+    domains: ['links.papareact.com'],
+  },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoibWF5eXVyIiwiYSI6ImNrczBzMzc5cTE0YWgydm1zamk4d3pvZm8ifQ.Z61voSSrKwmfm3mmLr_BDg',
+  },
+};
